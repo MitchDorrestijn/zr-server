b1bd410b57aea1bcb24e7c606b96d67ff5939353
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2018-04-20 12:53:44 +0200

    Fixed long line insert queries

--- resources/sql/insert_script.sql
+++ resources/sql/insert_script.sql
@@ -18,31 +18,19 @@ INSERT INTO `limitation` (name) VALUES
 
 INSERT INTO `user` (firstName, lastName, email, phoneNumber, street, houseNumber, zipCode, residence, password, PasswordSalt, dateOfBirth)
 VALUES
-  ('Wiedo', 'Harkema', 'Wiede@gmail.com', '0612345678', 'Kerkstraat', 7, '1234GB', 'Arnhem', 'wachtwoord', 'abc',
-            '1995-06-23'),
-  ('Robin', 'Schuiling', 'Robin@hotmail.com', '0687654321', 'Dorpstraat', 33, '1234EF', 'Arnhem', 'test', 'fgh',
-            '1997-10-07'),
-  ('Koen', 'van Keulen', 'koen@yahoo.com', '0678945612', 'Dorplaan', 12, '5673TR', 'Wageningen', 'pass', 'wer',
-           '1997-10-07'),
+  ('Wiedo', 'Harkema', 'Wiede@gmail.com', '0612345678', 'Kerkstraat', 7, '1234GB', 'Arnhem', 'wachtwoord', 'abc', '1995-06-23'),
+  ('Robin', 'Schuiling', 'Robin@hotmail.com', '0687654321', 'Dorpstraat', 33, '1234EF', 'Arnhem', 'test', 'fgh', '1997-10-07'),
+  ('Koen', 'van Keulen', 'koen@yahoo.com', '0678945612', 'Dorplaan', 12, '5673TR', 'Wageningen', 'pass', 'wer', '1997-10-07'),
   ('Hoang', 'Nguyen', 'hoang@outlook.com', '0645698712', 'Kruisweg', 7, '2845DF', 'Duiven', 'ww', 'cvb', '1992-02-25'),
-  ('Piet', 'Jansen', 'piet@jansen.nl', '0638458975', 'Gemeentelaan', 78, '4595HG', 'Nijmegen', 'sesamopenu', 'ghg',
-           '1958-12-13'),
-  ('Henk', 'Jansen', 'henk@jansen.nl', '0638458976', 'Dorpsweg', 78, '4597HG', 'Nijmegen', 'sesamopenu', 'ghg',
-           '1958-12-13'),
-  ('Jan', 'Jansen', 'jan@jansen.nl', '0638458977', 'tulpenlaan', 78, '4548HG', 'Nijmegen', 'sesamopenu', 'ghg',
-          '1958-12-13'),
-  ('Clemens', 'van Barneveld', 'clemens@vanbarneveld.net', '0613657226', 'Dorpsstraat', 83, '5673TR', 'Wageningen', 'sesamopenu', 'ghg',
-              '1958-12-13'),
-  ('Mitch', 'Zantingh', 'mitch@zantingh.net', '063515266', 'Dorpsstraat', 83, '6932HG', 'Westervoort', 'sesamopenu', 'ghg',
-            '1955-11-17'),
-  ('Abdirizak', 'Middelburg', 'abdirizakc@kpn.net', '0623458256', 'Johannes Husslaan', 13, '1185BK', 'Amstelveen', 'sesamopenu', 'ghg',
-                '1992-12-11'),
-  ('Kashif', 'Niewold', 'kashif@niewold.net', '0623458256', 'Hooicamp', 25, '3992BW', 'Houten', 'sesamopenu', 'ghg',
-             '1986-12-11'),
-  ('Iefke', 'Vloet', 'iefke@vloet.com', '0623458256', 'Haverakker', 102, '2743EJ', 'Waddinxveen', 'sesamopenu', 'ghg',
-            '1989-04-16'),
-  ('Mickey', 'van Zanten', 'mickey@tielmobiel.net', '0623458256', 'Marga Klompestraat', 140, '4133HN', 'Vianen', 'sesamopenu', 'ghg',
-             '1964-02-23');
+  ('Piet', 'Jansen', 'piet@jansen.nl', '0638458975', 'Gemeentelaan', 78, '4595HG', 'Nijmegen', 'sesamopenu', 'ghg', '1958-12-13'),
+  ('Henk', 'Jansen', 'henk@jansen.nl', '0638458976', 'Dorpsweg', 78, '4597HG', 'Nijmegen', 'sesamopenu', 'ghg', '1958-12-13'),
+  ('Jan', 'Jansen', 'jan@jansen.nl', '0638458977', 'tulpenlaan', 78, '4548HG', 'Nijmegen', 'sesamopenu', 'ghg', '1958-12-13'),
+  ('Clemens', 'van Barneveld', 'clemens@vanbarneveld.net', '0613657226', 'Dorpsstraat', 83, '5673TR', 'Wageningen', 'sesamopenu', 'ghg', '1958-12-13'),
+  ('Mitch', 'Zantingh', 'mitch@zantingh.net', '063515266', 'Dorpsstraat', 83, '6932HG', 'Westervoort', 'sesamopenu', 'ghg', '1955-11-17'),
+  ('Abdirizak', 'Middelburg', 'abdirizakc@kpn.net', '0623458256', 'Johannes Husslaan', 13, '1185BK', 'Amstelveen', 'sesamopenu', 'ghg', '1992-12-11'),
+  ('Kashif', 'Niewold', 'kashif@niewold.net', '0623458256', 'Hooicamp', 25, '3992BW', 'Houten', 'sesamopenu', 'ghg', '1986-12-11'),
+  ('Iefke', 'Vloet', 'iefke@vloet.com', '0623458256', 'Haverakker', 102, '2743EJ', 'Waddinxveen', 'sesamopenu', 'ghg', '1989-04-16'),
+  ('Mickey', 'van Zanten', 'mickey@tielmobiel.net', '0623458256', 'Marga Klompestraat', 140, '4133HN', 'Vianen', 'sesamopenu', 'ghg', '1964-02-23');
 
 INSERT INTO `client` (clientId, companion, utility, driverPreferenceForced) VALUES
   (2, 'Sven', 'rolstoel', FALSE),
@@ -130,30 +118,18 @@ INSERT INTO `drivercareinstitution` (driverId, careInstitutionId) VALUES
 
 INSERT INTO `ride` (clientId, driverId, pickUpDateTime, pickUpLocation, dropOffLocation, numberOfcompanions, numberOfLuggage, returnRide, callService, utility, repeatingRideId, cancelledByClient, executed, duration, distance)
 VALUES
-  (2, 1, '2018-01-03 12:00:00', 'Steenstraat 2, Arnhem', 'Kalverstraat 2, Amsterdam', 1, 0, TRUE, FALSE,
-      'rollator', 2, FALSE, TRUE, 30, 1500),
-  (2, 1, '2018-06-28 12:00:00', 'Ketelstraat 3, Arnhem', 'Coolsingen 3, Rotterdam', 0, 3, TRUE, FALSE,
-      'rollator', 2, FALSE, FALSE, 30, 6500),
-  (2, 1, '2018-03-03 12:00:00', 'Velperplein 4, Arnhem', 'Blaak 4, Rotterdam', 1, 0, TRUE, FALSE,
-      'rollator', 2, TRUE, FALSE, 30, 8500),
-  (2, 4, '2018-03-03 12:00:00', 'Barteljorisstraat 5, Haarlem', 'Vreeburg 5, Utrecht', 1, 2, TRUE, FALSE,
-      'rollator', 2, FALSE, FALSE, 30, 4000),
-  (3, 4, '2018-04-12 12:56:23', 'Steenstraat 6, Arnhem', 'Kalverstraat 6, Amsterdam', 1, 0, TRUE, FALSE,
-      'rollator', NULL, FALSE, FALSE, 30, 1568),
-  (6, NULL, '2018-06-03 16:00:00', 'Willemsplein 13, Arnhem', 'Dorpstraat 3, Kesteren', 0, 0, TRUE, FALSE,
-      'rollator', NULL, FALSE, FALSE, 30, 3000),
-  (7, NULL, '2018-02-03 16:00:00', 'Willemsplein 25, Arnhem', 'Nedereindsestraat 12, Kesteren', 0, 0, TRUE, FALSE,
-      'scootmobiel', NULL, FALSE, FALSE, 30, 5690),
-  (5, NULL, '2018-12-10 14:22:53', 'Willemsplein 5, Arnhem', 'Konings-plein 11, Amsterdam', 1, 4, TRUE, FALSE,
-      'scootmobiel', NULL, FALSE, FALSE, 30, 9850),
-  (5, NULL, '2018-12-10 14:22:53', 'Velperplein 5, Arnhem', 'Konings-plein 11, Amsterdam', 0, 0, TRUE, FALSE,
-      'scootmobiel', NULL, FALSE, FALSE, 30, 5670),
-  (3, NULL, '2018-11-06 14:22:53', 'Velperplein 5, Arnhem', 'Konings-plein 25, Amsterdam', 1, 0, TRUE, FALSE,
-      'scootmobiel', NULL, FALSE, FALSE, 30, 3200),
-  (6, NULL, '2018-12-08 09:16:00', 'Velperplein 5, Arnhem', 'Raamsteeg 46, Amsterdam', 1, 0, TRUE, FALSE,
-      'scootmobiel', NULL, FALSE, FALSE, 30, 3450),
-  (7, NULL, '2018-01-06 11:00:00', 'Steenstraat 8, Arnhem', 'Kalverstraat 8, Amsterdam', 0, 0, TRUE, FALSE,
-      'rollator', NULL, FALSE, FALSE, 30, 8650);
+  (2, 1, '2018-01-03 12:00:00', 'Steenstraat 2, Arnhem', 'Kalverstraat 2, Amsterdam', 1, 0, TRUE, FALSE, 'rollator', 2, FALSE, TRUE, 30, 1500),
+  (2, 1, '2018-06-28 12:00:00', 'Ketelstraat 3, Arnhem', 'Coolsingen 3, Rotterdam', 0, 3, TRUE, FALSE, 'rollator', 2, FALSE, FALSE, 30, 6500),
+  (2, 1, '2018-03-03 12:00:00', 'Velperplein 4, Arnhem', 'Blaak 4, Rotterdam', 1, 0, TRUE, FALSE, 'rollator', 2, TRUE, FALSE, 30, 8500),
+  (2, 4, '2018-03-03 12:00:00', 'Barteljorisstraat 5, Haarlem', 'Vreeburg 5, Utrecht', 1, 2, TRUE, FALSE, 'rollator', 2, FALSE, FALSE, 30, 4000),
+  (3, 4, '2018-04-12 12:56:23', 'Steenstraat 6, Arnhem', 'Kalverstraat 6, Amsterdam', 1, 0, TRUE, FALSE, 'rollator', NULL, FALSE, FALSE, 30, 1568),
+  (6, NULL, '2018-06-03 16:00:00', 'Willemsplein 13, Arnhem', 'Dorpstraat 3, Kesteren', 0, 0, TRUE, FALSE, 'rollator', NULL, FALSE, FALSE, 30, 3000),
+  (7, NULL, '2018-02-03 16:00:00', 'Willemsplein 25, Arnhem', 'Nedereindsestraat 12, Kesteren', 0, 0, TRUE, FALSE, 'scootmobiel', NULL, FALSE, FALSE, 30, 5690),
+  (5, NULL, '2018-12-10 14:22:53', 'Willemsplein 5, Arnhem', 'Konings-plein 11, Amsterdam', 1, 4, TRUE, FALSE, 'scootmobiel', NULL, FALSE, FALSE, 30, 9850),
+  (5, NULL, '2018-12-10 14:22:53', 'Velperplein 5, Arnhem', 'Konings-plein 11, Amsterdam', 0, 0, TRUE, FALSE, 'scootmobiel', NULL, FALSE, FALSE, 30, 5670),
+  (3, NULL, '2018-11-06 14:22:53', 'Velperplein 5, Arnhem', 'Konings-plein 25, Amsterdam', 1, 0, TRUE, FALSE, 'scootmobiel', NULL, FALSE, FALSE, 30, 3200),
+  (6, NULL, '2018-12-08 09:16:00', 'Velperplein 5, Arnhem', 'Raamsteeg 46, Amsterdam', 1, 0, TRUE, FALSE, 'scootmobiel', NULL, FALSE, FALSE, 30, 3450),
+  (7, NULL, '2018-01-06 11:00:00', 'Steenstraat 8, Arnhem', 'Kalverstraat 8, Amsterdam', 0, 0, TRUE, FALSE, 'rollator', NULL, FALSE, FALSE, 30, 8650);
 
 INSERT INTO `ridecanceledbydriver` (rideId, driverId, cancelledDate) VALUES
   (3, 1, '2018-01-01 12:00:00');
