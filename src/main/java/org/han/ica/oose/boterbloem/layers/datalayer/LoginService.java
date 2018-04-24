502f4efb8cf00b965b7cf62f2b929bb5e98c70ae
meesVDsloot <MR.vanderSloot@student.han.nl>
2018-04-24 09:49:34 +0200

    Pseudo code eruit omdat dat niet spring was

--- src/main/java/org/han/ica/oose/boterbloem/layers/datalayer/LoginService.java
+++ src/main/java/org/han/ica/oose/boterbloem/layers/datalayer/LoginService.java
@@ -2,21 +2,8 @@ package org.han.ica.oose.boterbloem.layers.datalayer;
 
 // @path("login")
 public class LoginService {
-    /*
-    pseudo code klopt nog niet, shit uit me vodagone dus neem het met een korrel zout
-    private UserDao userDao = new UserDao(//new DatabaseProperties())
 
-    @POST
-    @PRODUCES(Mediatype.APPLICATION_JSON)
-    @CONSUME(Mediatype.APPLICATION_JSON)
-    public Response login(LoginRequest login){
-       User user = userDao.login(login.user, login.password)
-        if (user == null) {
-         return Response.status(Response.Status.NOT_FOUND).build();
-         }
-         Loginresponse token = new LoginResponse(user.token,user.user);
-         return Response.status(200).entity(token).build();
-         }
+    /*
 
      */
 }
