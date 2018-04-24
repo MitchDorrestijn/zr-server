438929df4c87a254e5ff4912407df9ed7e71c18f
meesVDsloot <MR.vanderSloot@student.han.nl>
2018-04-24 11:13:10 +0200

    eerste opzet hashing van passwords. POM.xml security van spring

---
+++ src/main/java/org/han/ica/oose/boterbloem/PasswordEncoder.java
@@ -0,0 +1,13 @@
+package org.han.ica.oose.boterbloem;
+
+/*
+staat nog niet in de goede layer, moet later nog wel gebeuren
+Er wordt gebruik gemaakt van BCrypt
+ alternatieven zijn MD5PasswordEncoder en ShaPasswordEncoder, deze zijn deprecated
+ */
+
+import org.springframework.context.annotation.Bean;
+
+public interface PasswordEncoder {
+
+}
---
+++ src/main/java/org/han/ica/oose/boterbloem/PasswordEncoderGenerator.java
@@ -0,0 +1,15 @@
+package org.han.ica.oose.boterbloem;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderGenerator {
+    public static void main(String[] args) {
+        int i =0;
+        while (i<10){
+            String password = "123456";
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String hasedPassword = passwordEncoder.encode(password);
+            System.out.println(hasedPassword);
+            i++;
+        }
+    }
+}
