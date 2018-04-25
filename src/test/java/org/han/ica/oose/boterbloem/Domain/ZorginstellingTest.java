d3c926a9162fc5cc9eb2bf063e130ae0869bdb59
Tim Kruijf <T.deKruijf@student.han.nl>
2018-04-25 15:49:14 +0200

    Test class for domain Zorginstelling

---
+++ src/test/java/org/han/ica/oose/boterbloem/Domain/ZorginstellingTest.java
@@ -0,0 +1,41 @@
+package org.han.ica.oose.boterbloem.Domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZorginstellingTest {
+    Zorginstelling zorginstelling;
+
+    @Before
+    public void setUp() throws Exception {
+        zorginstelling = new Zorginstelling(1, "Ziekenhuis de prothese");
+    }
+
+    @Test
+    public void getId() {
+        assertEquals(1, zorginstelling.getId());
+        assertNotEquals(2, zorginstelling.getId());
+    }
+
+    @Test
+    public void setId() {
+        zorginstelling.setId(2);
+        assertEquals(2, zorginstelling.getId());
+        assertNotEquals(1, zorginstelling.getId());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Ziekenhuis de prothese", zorginstelling.getName());
+        assertNotEquals("Ziekenhuis de prothes", zorginstelling.getName());
+    }
+
+    @Test
+    public void setName() {
+        zorginstelling.setName("Ziekenhuis de prothes");
+        assertEquals("Ziekenhuis de prothes", zorginstelling.getName());
+        assertNotEquals("Ziekenhuis de prothese", zorginstelling.getName());
+    }
+}
