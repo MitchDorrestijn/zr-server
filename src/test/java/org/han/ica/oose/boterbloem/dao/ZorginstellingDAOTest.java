38c595461341e8cf25d4550065511baa84674850
Tim Kruijf <T.deKruijf@student.han.nl>
2018-04-26 12:13:38 +0200

    Empty Test class for ZorginstellingDAO

---
+++ src/test/java/org/han/ica/oose/boterbloem/dao/ZorginstellingDAOTest.java
@@ -0,0 +1,37 @@
+package org.han.ica.oose.boterbloem.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZorginstellingDAOTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void getAllZorginstellingen() {
+    }
+
+    @Test
+    public void getByID() {
+    }
+
+    @Test
+    public void getByName() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void deleteByName() {
+    }
+}
