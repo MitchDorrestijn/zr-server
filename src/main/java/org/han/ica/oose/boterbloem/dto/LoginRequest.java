4283b35e8b97691a1d1f28f3ffc35533c6290cd2
Asir Shapour <MacBook-Air@MacBook-Air.local>
2018-04-26 14:03:45 +0200

    dto for login

--- src/main/java/org/han/ica/oose/boterbloem/dto/LoginRequest.java
+++ src/main/java/org/han/ica/oose/boterbloem/dto/LoginRequest.java
@@ -1,4 +1,25 @@
 package org.han.ica.oose.boterbloem.dto;
 
+/**
+ * Representative DTO for a LoginRequest
+ */
 public class LoginRequest {
+    private String username;
+    private String password;
+
+    /**
+     * Getter for property 'username'.
+     * @return Value for property 'username'.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Getter for propery 'password'
+     * @return Value for property 'password'
+     */
+    public String getPassword() {
+        return password;
+    }
 }
--- src/main/java/org/han/ica/oose/boterbloem/dto/LoginResponse.java
+++ src/main/java/org/han/ica/oose/boterbloem/dto/LoginResponse.java
@@ -1,4 +1,70 @@
 package org.han.ica.oose.boterbloem.dto;
 
+/**
+ * Representative DTO for a LoginResponse
+ */
 public class LoginResponse {
+    private String user;
+    private String token;
+    private String administratorLevel;
+
+    public LoginResponse(String user, String token, String administratorLevel) {
+        this.user = user;
+        this.token = token;
+        this.administratorLevel = administratorLevel;
+    }
+
+    /**
+     * Getter for propery 'user'
+     *
+     * @return Value for property 'user'
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * Setter for property 'user'.
+     *
+     * @param user Value to set for property 'user'.
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * Getter for property 'token'
+     *
+     * @return Value for property 'token'
+     */
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * Setter for property 'token'
+     *
+     * @param token Value to set for property 'token'
+     */
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    /**
+     * Getter for property 'administratorLevel'
+     *
+     * @return Value for property 'administratorLevel'
+     */
+    public String getAdministratorLevel() {
+        return administratorLevel;
+    }
+
+    /**
+     * Setter for property 'administratorLevel'
+     *
+     * @param administratorLevel Value to set property 'administratorLevel'
+     */
+    public void setAdministratorLevel(String administratorLevel) {
+        this.administratorLevel = administratorLevel;
+    }
 }
