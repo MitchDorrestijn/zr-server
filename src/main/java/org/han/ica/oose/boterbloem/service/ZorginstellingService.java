d5762e3763461bb95e5313eb6057bb204f2d15bb
Martijn Hulshof <M.Hulshof@student.han.nl>
2018-05-08 13:30:45 +0200

    fixed service

--- src/main/java/org/han/ica/oose/boterbloem/service/ZorginstellingService.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/ZorginstellingService.java
@@ -5,13 +5,15 @@ import org.han.ica.oose.boterbloem.domain.Zorginstelling;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
-public class ZorginstellingService {
+public class ZorginstellingService  {
+    ZorginstellingDAO DAO = new ZorginstellingDAO();
 
-    public ZorginstellingService() {
+
+    public ZorginstellingService(){
         // Empty constructor
     }
-
     /**
      * Finds and returns a Zorginstelling using the given ID
      *
@@ -33,8 +35,15 @@ public class ZorginstellingService {
         DAO.updateZorginstellingWithId(zorginstelling.getId(), zorginstelling.getName()); // hij moet de geposte naam hier zetten
     }
 
-
     /**
+     *
+     * @param zorginstelling thats going to be added to the database
+     */
+    public void saveZorginstelling(Zorginstelling zorginstelling) {
+        DAO.create(zorginstelling);
+    }
+    /**
+     *
      * @return List of all zorginstellingen
      */
     public List<Zorginstelling> getAllZorginstellingen() throws SQLException {
