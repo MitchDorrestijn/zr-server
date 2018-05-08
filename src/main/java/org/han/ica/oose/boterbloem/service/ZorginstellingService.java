f011f30389a840b664b9caf5b4f3d22e2289c200
Tim Kruijf <T.deKruijf@student.han.nl>
2018-05-08 13:07:54 +0200

    Undo refactoring because of a jenkins issue

--- src/main/java/org/han/ica/oose/boterbloem/service/ZorginstellingService.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/ZorginstellingService.java
@@ -3,47 +3,43 @@ package org.han.ica.oose.boterbloem.service;
 import org.han.ica.oose.boterbloem.dao.ZorginstellingDAO;
 import org.han.ica.oose.boterbloem.domain.Zorginstelling;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class ZorginstellingService {
-    ZorginstellingDAO zorginstellingDAO = new ZorginstellingDAO();
-
 
     public ZorginstellingService(){
         // Empty constructor
     }
+
     /**
      * Finds and returns a Zorginstelling using the given ID
      *
      * @param id ID of the zorginstelling
      * @return The zorginstelling that was found using the ID
+     * @throws SQLException
      */
-    public Zorginstelling findById(int id) {
-        ZorginstellingDAO dao = new ZorginstellingDAO();
-        return (dao.getByID(id));
+    public Zorginstelling findById(int id) throws SQLException {
+        ZorginstellingDAO DAO = new ZorginstellingDAO();
+        return (DAO.getByID(id));
     }
 
     /**
      * @param zorginstelling Zorgstelling that needs a update
+     * @throws SQLException
      */
-    public void updateZorginstelling(Zorginstelling zorginstelling)  {
-        ZorginstellingDAO dao = new ZorginstellingDAO();
-        dao.updateZorginstellingWithId(zorginstelling.getId(), zorginstelling.getName()); // hij moet de geposte naam hier zetten
+    public void updateZorginstelling(Zorginstelling zorginstelling) throws SQLException {
+        ZorginstellingDAO DAO = new ZorginstellingDAO();
+        DAO.updateZorginstellingWithId(zorginstelling.getId(), zorginstelling.getName()); // hij moet de geposte naam hier zetten
     }
 
-    /**
-     *
-     * @param zorginstelling thats going to be added to the database
-     */
-    public void saveZorginstelling(Zorginstelling zorginstelling) {
-        zorginstellingDAO.create(zorginstelling);
-    }
+
     /**
      *
      * @return List of all zorginstellingen
      */
-    public List<Zorginstelling> getAllZorginstellingen() {
-        ZorginstellingDAO dao = new ZorginstellingDAO();
-        return dao.getAllZorginstellingen();
+    public List<Zorginstelling> getAllZorginstellingen() throws SQLException {
+        ZorginstellingDAO DAO = new ZorginstellingDAO();
+        return DAO.getAllZorginstellingen();
     }
 }
