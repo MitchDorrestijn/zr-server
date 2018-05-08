81d76d015e25da161c2879adb0cd64ff39ece2ad
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2018-05-08 15:04:44 +0200

    Added CrossOrigin for CORS fix

--- src/main/java/org/han/ica/oose/boterbloem/controller/ZorginstellingController.java
+++ src/main/java/org/han/ica/oose/boterbloem/controller/ZorginstellingController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.logging.Level;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/zorginstelling")
 public class ZorginstellingController {
 
