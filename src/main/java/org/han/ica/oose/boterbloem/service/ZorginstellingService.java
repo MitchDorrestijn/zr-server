97e781d69ce4f128b259257b4b2897b97bf28e1a
Tim Kruijf <T.deKruijf@student.han.nl>
2018-05-08 13:01:49 +0200

    More sonar issues

--- src/main/java/org/han/ica/oose/boterbloem/service/ZorginstellingService.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/ZorginstellingService.java
@@ -3,12 +3,10 @@ package org.han.ica.oose.boterbloem.service;
 import org.han.ica.oose.boterbloem.dao.ZorginstellingDAO;
 import org.han.ica.oose.boterbloem.domain.Zorginstelling;
 
-import java.sql.SQLException;
 import java.util.List;
-import java.util.Objects;
 
 public class ZorginstellingService {
-    ZorginstellingDAO DAO = new ZorginstellingDAO();
+    ZorginstellingDAO zorginstellingDAO = new ZorginstellingDAO();
 
 
     public ZorginstellingService(){
@@ -19,7 +17,6 @@ public class ZorginstellingService {
      *
      * @param id ID of the zorginstelling
      * @return The zorginstelling that was found using the ID
-     * @throws SQLException
      */
     public Zorginstelling findById(int id) {
         ZorginstellingDAO dao = new ZorginstellingDAO();
@@ -28,7 +25,6 @@ public class ZorginstellingService {
 
     /**
      * @param zorginstelling Zorgstelling that needs a update
-     * @throws SQLException
      */
     public void updateZorginstelling(Zorginstelling zorginstelling)  {
         ZorginstellingDAO dao = new ZorginstellingDAO();
@@ -40,7 +36,7 @@ public class ZorginstellingService {
      * @param zorginstelling thats going to be added to the database
      */
     public void saveZorginstelling(Zorginstelling zorginstelling) {
-        DAO.create(zorginstelling);
+        zorginstellingDAO.create(zorginstelling);
     }
     /**
      *
