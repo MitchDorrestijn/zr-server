d557fcd5dbfa3aaea6f418ccff0aed9b155fdd8e
Mitch Dorrestijn <M.Dorrestijn1@student.han.nl>
2018-05-14 14:42:12 +0200

    Added service layer for the chauffeurs

--- src/main/java/org/han/ica/oose/boterbloem/service/ChauffeurService.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/ChauffeurService.java
@@ -1,4 +1,19 @@
 package org.han.ica.oose.boterbloem.service;
 
-public class ChauffeurService {
+import org.han.ica.oose.boterbloem.dao.ChauffeurDAO;
+import org.han.ica.oose.boterbloem.domain.Chauffeur;
+
+import java.util.List;
+
+public class ChauffeurService implements IChauffeurService {
+    ChauffeurDAO chauffeurDAO = new ChauffeurDAO();
+
+    public ChauffeurService() {
+    }
+
+    @Override
+    public List<Chauffeur> getAllChauffeurs() {
+        return chauffeurDAO.getAllChauffeurs();
+    }
+
 }
--- src/main/java/org/han/ica/oose/boterbloem/service/IChauffeurService.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/IChauffeurService.java
@@ -1,4 +1,9 @@
 package org.han.ica.oose.boterbloem.service;
 
+import org.han.ica.oose.boterbloem.domain.Chauffeur;
+
+import java.util.List;
+
 public interface IChauffeurService {
+    List<Chauffeur> getAllChauffeurs();
 }
