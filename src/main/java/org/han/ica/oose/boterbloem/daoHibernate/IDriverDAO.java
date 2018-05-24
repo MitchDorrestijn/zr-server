fe2ae7f5b353cab0ed5521594f11189418c52343
Mitch Dorrestijn <M.Dorrestijn1@student.han.nl>
2018-05-24 11:31:07 +0200

    Fixed merge conflicts in IDriverDAO.java

--- src/main/java/org/han/ica/oose/boterbloem/daoHibernate/IDriverDAO.java
+++ src/main/java/org/han/ica/oose/boterbloem/daoHibernate/IDriverDAO.java
@@ -6,10 +6,6 @@ import org.han.ica.oose.boterbloem.entity.DrivercarEntity;
 import org.han.ica.oose.boterbloem.entity.DrivercarEntityPK;
 
 public interface IDriverDAO extends IGenericDAO<DriverEntity> {
-<<<<<<< HEAD
-
     int latestId();
-=======
     void deleteDriver(int driverId);
->>>>>>> 6064b35895394752e735eb28ee0b23d7a80b3147
 }
