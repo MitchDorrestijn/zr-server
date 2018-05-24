d03501dacdc136b48d0e84ffb7bb36da1d5b28b4
Asir Shapour <MacBook-Air@ws-148-205.han.nl>
2018-05-24 18:49:14 +0200

    refactor clientDetailDisplay & updateClient

--- src/main/java/org/han/ica/oose/boterbloem/service/projection/ClientDetailDisplay.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/projection/ClientDetailDisplay.java
@@ -1,332 +1,46 @@
 package org.han.ica.oose.boterbloem.service.projection;
 
+import org.han.ica.oose.boterbloem.entity.ClientEntity;
 
-import java.util.Date;
-import java.util.List;
+import java.util.ArrayList;
 
 public class ClientDetailDisplay {
-    private int id;
-    private String image;
-    private String firstName;
-    private String lastName;
-    private Date dateOfBirth;
-    private String street;
-    private String houseNumber;
-    private String zipCode;
-
-    private String residence;
-    private String email;
-    private String phoneNumber;
-    private String bankAccount;
-    private String password;
-    private byte companionRequired;
-    private List<String> clientlimitationEntityList;
-    private int personalKmBudget;
-    private int totalRides;
+    private ClientEntity client;
+    private ArrayList <String> limitations;
 
     /**
-     * Getter for property 'id'.
+     * Getter for property 'client'.
      *
-     * @return Value for property 'id'.
+     * @return Value for property 'client'.
      */
-    public int getId() {
-        return id;
+    public ClientEntity getClient() {
+        return client;
     }
 
     /**
-     * Setter for property 'id'.
+     * Setter for property 'client'.
      *
-     * @param id Value to set for property 'id'.
+     * @param client Value to set for property 'client'.
      */
-    public void setId(int id) {
-        this.id = id;
+    public void setClient(ClientEntity client) {
+        this.client = client;
     }
 
     /**
-     * Getter for property 'image'.
+     * Getter for property 'limitations'.
      *
-     * @return Value for property 'image'.
+     * @return Value for property 'limitations'.
      */
-    public String getImage() {
-        return image;
+    public ArrayList <String> getLimitations() {
+        return limitations;
     }
 
     /**
-     * Setter for property 'image'.
+     * Setter for property 'limitations'.
      *
-     * @param image Value to set for property 'image'.
+     * @param limitations Value to set for property 'limitations'.
      */
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    /**
-     * Getter for property 'firstName'.
-     *
-     * @return Value for property 'firstName'.
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * Setter for property 'firstName'.
-     *
-     * @param firstName Value to set for property 'firstName'.
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Getter for property 'lastName'.
-     *
-     * @return Value for property 'lastName'.
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * Setter for property 'lastName'.
-     *
-     * @param lastName Value to set for property 'lastName'.
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Getter for property 'dateOfBirth'.
-     *
-     * @return Value for property 'dateOfBirth'.
-     */
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    /**
-     * Setter for property 'dateOfBirth'.
-     *
-     * @param dateOfBirth Value to set for property 'dateOfBirth'.
-     */
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    /**
-     * Getter for property 'street'.
-     *
-     * @return Value for property 'street'.
-     */
-    public String getStreet() {
-        return street;
-    }
-
-    /**
-     * Setter for property 'street'.
-     *
-     * @param street Value to set for property 'street'.
-     */
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    /**
-     * Getter for property 'houseNumber'.
-     *
-     * @return Value for property 'houseNumber'.
-     */
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    /**
-     * Setter for property 'houseNumber'.
-     *
-     * @param houseNumber Value to set for property 'houseNumber'.
-     */
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
-    /**
-     * Getter for property 'zipCode'.
-     *
-     * @return Value for property 'zipCode'.
-     */
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    /**
-     * Setter for property 'zipCode'.
-     *
-     * @param zipCode Value to set for property 'zipCode'.
-     */
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    /**
-     * Getter for property 'residence'.
-     *
-     * @return Value for property 'residence'.
-     */
-    public String getResidence() {
-        return residence;
-    }
-
-    /**
-     * Setter for property 'residence'.
-     *
-     * @param residence Value to set for property 'residence'.
-     */
-    public void setResidence(String residence) {
-        this.residence = residence;
-    }
-
-    /**
-     * Getter for property 'email'.
-     *
-     * @return Value for property 'email'.
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Setter for property 'email'.
-     *
-     * @param email Value to set for property 'email'.
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * Getter for property 'phoneNumber'.
-     *
-     * @return Value for property 'phoneNumber'.
-     */
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * Setter for property 'phoneNumber'.
-     *
-     * @param phoneNumber Value to set for property 'phoneNumber'.
-     */
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * Getter for property 'bankAccount'.
-     *
-     * @return Value for property 'bankAccount'.
-     */
-    public String getBankAccount() {
-        return bankAccount;
-    }
-
-    /**
-     * Setter for property 'bankAccount'.
-     *
-     * @param bankAccount Value to set for property 'bankAccount'.
-     */
-    public void setBankAccount(String bankAccount) {
-        this.bankAccount = bankAccount;
-    }
-
-    /**
-     * Getter for property 'password'.
-     *
-     * @return Value for property 'password'.
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * Setter for property 'password'.
-     *
-     * @param password Value to set for property 'password'.
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * Getter for property 'companionRequired'.
-     *
-     * @return Value for property 'companionRequired'.
-     */
-    public byte getCompanionRequired() {
-        return companionRequired;
-    }
-
-    /**
-     * Setter for property 'companionRequired'.
-     *
-     * @param companionRequired Value to set for property 'companionRequired'.
-     */
-    public void setCompanionRequired(byte companionRequired) {
-        this.companionRequired = companionRequired;
-    }
-
-    /**
-     * Getter for property 'clientlimitationEntityList'.
-     *
-     * @return Value for property 'clientlimitationEntityList'.
-     */
-    public List <String> getClientlimitationEntityList() {
-        return clientlimitationEntityList;
-    }
-
-    /**
-     * Setter for property 'clientlimitationEntityList'.
-     *
-     * @param clientlimitationEntityList Value to set for property 'limitationEntityList'.
-     */
-    public void setClientlimitationEntityList(List <String> clientlimitationEntityList) {
-        this.clientlimitationEntityList = clientlimitationEntityList;
-    }
-
-    /**
-     * Getter for property 'personalKmBudget'.
-     *
-     * @return Value for property 'personalKmBudget'.
-     */
-    public int getPersonalKmBudget() {
-        return personalKmBudget;
-    }
-
-    /**
-     * Setter for property 'personalKmBudget'.
-     *
-     * @param personalKmBudget Value to set for property 'personalKmBudget'.
-     */
-    public void setPersonalKmBudget(int personalKmBudget) {
-        this.personalKmBudget = personalKmBudget;
-    }
-
-    /**
-     * Getter for property 'totalRides'.
-     *
-     * @return Value for property 'totalRides'.
-     */
-    public int getTotalRides() {
-        return totalRides;
-    }
-
-    /**
-     * Setter for property 'totalRides'.
-     *
-     * @param totalRides Value to set for property 'totalRides'.
-     */
-    public void setTotalRides(int totalRides) {
-        this.totalRides = totalRides;
+    public void setLimitations(ArrayList <String> limitations) {
+        this.limitations = limitations;
     }
 }
