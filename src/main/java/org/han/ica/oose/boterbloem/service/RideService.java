11de25880444cec76445ebfcaf219fa480b1a625
Mitch Dorrestijn <M.Dorrestijn1@student.han.nl>
2018-05-29 14:20:34 +0200

    Created ride service

--- src/main/java/org/han/ica/oose/boterbloem/service/RideService.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/RideService.java
@@ -1,13 +1,41 @@
 package org.han.ica.oose.boterbloem.service;
 
+import org.han.ica.oose.boterbloem.daoHibernate.IRideDAO;
+import org.han.ica.oose.boterbloem.daoHibernate.RideDAOImpl;
+import org.han.ica.oose.boterbloem.entity.RideEntity;
 import org.han.ica.oose.boterbloem.service.displays.RideOverviewDisplay;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RideService implements IRideService {
+    private static final Logger LOGGER = Logger.getLogger(DriverService.class.getName());
+    private IRideDAO rideDAO = new RideDAOImpl();
 
     @Override
     public List<RideOverviewDisplay> getRideOverview() {
-        return null;
+        List<RideOverviewDisplay> returnList = new ArrayList<>();
+        for (RideEntity r: rideDAO.findAll()) {
+            try {
+                RideOverviewDisplay rideOverviewDisplay = new RideOverviewDisplay();
+                rideOverviewDisplay.setPaymentStatus(r.getPaymentStatus());
+                try {
+                    rideOverviewDisplay.setDriverName(r.getDriverEntity().getUserEntity().getFirstName());
+                    rideOverviewDisplay.setAccountnr(r.getDriverEntity().getAccountnr());
+                } catch (NullPointerException e) {
+                    rideOverviewDisplay.setDriverName("Geen chauffeur gevonden");
+                    rideOverviewDisplay.setAccountnr("Geen bankrekening gevonden");
+                }
+                rideOverviewDisplay.setPrice_of_ride(r.getPriceOfRide());
+                rideOverviewDisplay.setPaymentDescription(r.getPaymentDescription());
+                rideOverviewDisplay.setPaymentDueBefore(r.getPaymentDueBefore());
+                returnList.add(rideOverviewDisplay);
+            } catch (Exception e){
+                LOGGER.log(Level.WARNING,e.getMessage());
+            }
+        }
+        return returnList;
     }
 }
--- src/main/java/org/han/ica/oose/boterbloem/service/displays/RideOverviewDisplay.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/displays/RideOverviewDisplay.java
@@ -1,41 +1,118 @@
 package org.han.ica.oose.boterbloem.service.displays;
 
-import org.han.ica.oose.boterbloem.entity.DriverEntity;
-import org.han.ica.oose.boterbloem.entity.RideEntity;
-
 public class RideOverviewDisplay {
-    private DriverEntity driverEntity;
-    private RideEntity rideEntity;
+    private String driverName;
+    private String accountnr;
+    private float price_of_ride;
+    private String paymentDescription;
+    private String paymentStatus;
+    private String paymentDueBefore;
 
     /**
-     * Getter for the property driver
-     * @return driverEntity
+     * Getter for property 'driverName'.
+     *
+     * @return Value for property 'driverName'.
      */
-    public DriverEntity getDriverEntity() {
-        return driverEntity;
+    public String getDriverName() {
+        return driverName;
     }
 
     /**
-     * Setter for the property driver
-     * @param driverEntity Value to set for property 'driverEntity'.
+     * Setter for property 'driverName'.
+     *
+     * @param driverName Value to set for property 'driverName'.
      */
-    public void setDriverEntity(DriverEntity driverEntity) {
-        this.driverEntity = driverEntity;
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
     }
 
     /**
-     * Getter for the property ride
-     * @return rideEntity
+     * Getter for property 'accountnr'.
+     *
+     * @return Value for property 'accountnr'.
      */
-    public RideEntity getRideEntity() {
-        return rideEntity;
+    public String getAccountnr() {
+        return accountnr;
     }
 
     /**
-     * Setter for the property ride
-     * @param rideEntity Value to set for property 'rideEntity'.
+     * Setter for property 'accountnr'.
+     *
+     * @param accountnr Value to set for property 'accountnr'.
      */
-    public void setRideEntity(RideEntity rideEntity) {
-        this.rideEntity = rideEntity;
+    public void setAccountnr(String accountnr) {
+        this.accountnr = accountnr;
+    }
+
+    /**
+     * Getter for property 'price_of_ride'.
+     *
+     * @return Value for property 'price_of_ride'.
+     */
+    public float getPrice_of_ride() {
+        return price_of_ride;
+    }
+
+    /**
+     * Setter for property 'price_of_ride'.
+     *
+     * @param price_of_ride Value to set for property 'price_of_ride'.
+     */
+    public void setPrice_of_ride(float price_of_ride) {
+        this.price_of_ride = price_of_ride;
+    }
+
+    /**
+     * Getter for property 'paymentDescription'.
+     *
+     * @return Value for property 'paymentDescription'.
+     */
+    public String getPaymentDescription() {
+        return paymentDescription;
+    }
+
+    /**
+     * Setter for property 'paymentDescription'.
+     *
+     * @param paymentDescription Value to set for property 'paymentDescription'.
+     */
+    public void setPaymentDescription(String paymentDescription) {
+        this.paymentDescription = paymentDescription;
+    }
+
+    /**
+     * Getter for property 'paymentStatus'.
+     *
+     * @return Value for property 'paymentStatus'.
+     */
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    /**
+     * Setter for property 'paymentStatus'.
+     *
+     * @param paymentStatus Value to set for property 'paymentStatus'.
+     */
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    /**
+     * Getter for property 'paymentDueBefore'.
+     *
+     * @return Value for property 'paymentDueBefore'.
+     */
+    public String getPaymentDueBefore() {
+        return paymentDueBefore;
+    }
+
+    /**
+     * Setter for property 'paymentDueBefore'.
+     *
+     * @param paymentDueBefore Value to set for property 'paymentDueBefore'.
+     */
+    public void setPaymentDueBefore(String paymentDueBefore) {
+        this.paymentDueBefore = paymentDueBefore;
     }
 }
