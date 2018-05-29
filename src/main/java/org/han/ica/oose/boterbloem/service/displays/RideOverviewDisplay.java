c53685fea7bcc96bda89f93fb8945ba033c4f026
Mitch Dorrestijn <M.Dorrestijn1@student.han.nl>
2018-05-29 12:11:57 +0200

    Filled the display for RideOverview

--- src/main/java/org/han/ica/oose/boterbloem/service/displays/RideOverviewDisplay.java
+++ src/main/java/org/han/ica/oose/boterbloem/service/displays/RideOverviewDisplay.java
@@ -1,4 +1,41 @@
 package org.han.ica.oose.boterbloem.service.displays;
 
+import org.han.ica.oose.boterbloem.entity.DriverEntity;
+import org.han.ica.oose.boterbloem.entity.RideEntity;
+
 public class RideOverviewDisplay {
+    private DriverEntity driverEntity;
+    private RideEntity rideEntity;
+
+    /**
+     * Getter for the property driver
+     * @return driverEntity
+     */
+    public DriverEntity getDriverEntity() {
+        return driverEntity;
+    }
+
+    /**
+     * Setter for the property driver
+     * @param driverEntity Value to set for property 'driverEntity'.
+     */
+    public void setDriverEntity(DriverEntity driverEntity) {
+        this.driverEntity = driverEntity;
+    }
+
+    /**
+     * Getter for the property ride
+     * @return rideEntity
+     */
+    public RideEntity getRideEntity() {
+        return rideEntity;
+    }
+
+    /**
+     * Setter for the property ride
+     * @param rideEntity Value to set for property 'rideEntity'.
+     */
+    public void setRideEntity(RideEntity rideEntity) {
+        this.rideEntity = rideEntity;
+    }
 }
